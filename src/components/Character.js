81074bc7e95aba97cbc4aaf83157f5b537f93a74
eli-kirschbaum <eli.kirschbaum1@gmail.com>
2022-02-18 17:41:31 -0600

    made a boilerplate character component and gave it props

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,22 @@
 // Write your Character component here
+import React from 'react';
+
+
+const Character = (props) => {
+
+    const { person } = props
+    
+    return (
+        <div className='character-container'>
+            <h2>NAME GOES HERE</h2>
+            <p>GENDER GOES HERE</p>
+            <p>AGE GOES HERE</p>
+            <p>BIRTHYEAR GOES HERE</p>
+            <p>HEIGHT</p>
+            <p>MASS</p>
+        </div>
+    )
+
+}
+
+export default Character
