416fc8e8b0112d036c94c59cd0a478a7ab5b386f
eli-kirschbaum <eli.kirschbaum1@gmail.com>
2022-03-02 01:54:37 -0600

    edited to make all tests pass and added some basic styling via styled components

--- src/App.js
+++ src/App.js
@@ -3,58 +3,36 @@ import './App.css';
 import axios from 'axios';
 import Character from './components/Character'
 
+
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [peopleUrls, setPeopleUrls] = useState([]);
+  
   const [people, setPeople] = useState([]);
+  
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
   useEffect(() => {
-    
-
-      axios.get('https://swapi.dev/api/films')
+  console.log('TEST TEST TEST')
+    axios.get('https://swapi.dev/api/people')
       .then(res => {
-        //console.log(res.data.results)
-        res.data.results.forEach(movie => {
-          setPeopleUrls(movie.characters)
-        })
-      })
-      .catch(err => console.error(err))
-
-      //console.log(peopleUrls)
-  }, [])
-
-  const getPersonInfo = () => {
+        console.log(res.data)
+        setPeople(res.data.results ?? res.data)
+      }).catch(error => {throw 'ERROR ERROR ERROR'})
     
-    peopleUrls.forEach(url => {
-      axios.get(url)
-        .then(res =>{
-          setPeople(res)
-        })
-        .catch(err => console.error(err))
-
-        console.log(people)
-    })
-    
-
-    }
-  
-
-
-
-  
-  
-  
+  }, []);
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1>Characters of Star Wars</h1>
+        {
           
-        <Character info={people} />
-
+        people?.map(person => {
+          return <Character key={person.name} person={person}/>
+        })
+        }
 
 
     </div>
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,23 +1,42 @@
 // Write your Character component here
 import React from 'react';
+import styled from 'styled-components';
 
+const StyledCharacterCard = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 45%;
+    justify-content: space-between;
+    padding: .5px;
+    margin: 3px;
+    border: 2px solid #DAA520;
+    background-color: coral;
+`
 
 const Character = (props) => {
 
-    const { person } = props
+    const { person } = props;
+    const { name, gender, birth_year, height, mass } = person;
 
     return (
-        <div className='character-container'>
-            <h2>{person.name}</h2>
-            <div className='character-stats'>
-                <p>Gender: {person.gender}</p>
-                <p>Birth-Year: {person.birth_year}</p>
-                <p>Height: {person.height}</p>
-                <p>Mass: {person.mass}</p>
-            </div>  
-        </div>
+        <StyledCharacterCard>
+            <article key={`App-peopleMap-personId-${person.id}`}>
+            {name}
+                <p>Gender: {gender}</p>
+                <p>Birth-Year: {birth_year}</p>
+                <p>Height: {height}</p>
+                <p>Mass: {mass}</p>
+        </article>
+
+
+        </StyledCharacterCard>
+        
+        
     )
+    
 
 }
 
-export default Character
+export default Character;
+
+
